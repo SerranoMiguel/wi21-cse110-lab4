9648aec220b22112dbced67472c59b24b0538360
Miguel Serrano <maserran@ucsd.edu>
2021-02-20 20:59:59 -0800

    Work

---
+++ part1/part1-question18.js
@@ -0,0 +1,24 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2
+}
+ 
+
+for (key in statistics) {
+  if(statistics[key] %2 != 0 ||
+}
+// using a loop, iterate through it and print out the value of the property
+// of the property starts w/ letter r or if that property is odd
+/* so, print value of property if:
+   -property starts with "r" OR
+   - property value is odd
+   - Should be printing:
+    21
+    45
+    5
+    2
+   */
